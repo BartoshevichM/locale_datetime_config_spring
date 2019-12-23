5180f55eb61ff34e25e4de47a19e7c2649ce4e35
bartoshevichm <bartoshevich.m@gmail.com>
2019-12-23 09:52:33 +0200

    configuration local file

---
+++ LocaleConfig.java
@@ -0,0 +1,13 @@
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@Configuration
+public class LocaleConfig {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+}
